2fa06d36b770cd52147ec046134bf82ae13b0f94
Verchurro <berrygirault@gmail.com>
2023-01-27 11:18:04 +0100

    updateee

--- CashierSim/Assets/Core/Scripts/Spawner.cs
+++ CashierSim/Assets/Core/Scripts/Spawner.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject itemPrefab;
+    public List<GameObject> itemsToSpawn = new List<GameObject>();
     public float respawnTime = 1.0f;
     private Vector2 screenbounds;
+    public bool isRandomized;
 
     private void Start()
     {
         screenbounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(itemWave());
     }
     private void spawnItems()
     {
-        GameObject a = Instantiate(itemPrefab) as GameObject;
+        int index = isRandomized ? Random.Range(0, itemsToSpawn.Count) : 0;
+
+        GameObject a = Instantiate(itemsToSpawn) as GameObject;
+        a.transform.position = new Vector3(screenbounds.x * 0, 5, 0);
+    }
+
+    IEnumerator itemWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnItems();
+        }
+
     }
 }
