40cb9c01277567c8e81a612ac36823222cba0fe7
Verchurro <berrygirault@gmail.com>
2023-02-03 12:16:46 +0100

    Random spawn fix

--- CashierSim/Assets/Core/Scripts/Spawner.cs
+++ CashierSim/Assets/Core/Scripts/Spawner.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject itemPrefab;
+    public GameObject[] itemsToSpawn;
     public float respawnTime = 1.0f;
     private Vector2 screenbounds;
+    public bool isRandomized;
 
     private void Start()
     {
         screenbounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(itemWave());
     }
     private void spawnItems()
     {
-        GameObject a = Instantiate(itemPrefab) as GameObject;
+        int randomIndex = Random.Range(0, itemsToSpawn.Length);
+
+        Instantiate(itemsToSpawn[randomIndex]);
+        Vector3 spawnPosition = new Vector3(screenbounds.x * 0, 5, 0);
+    }
+
+    IEnumerator itemWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnItems();
+        }
+
     }
 }
