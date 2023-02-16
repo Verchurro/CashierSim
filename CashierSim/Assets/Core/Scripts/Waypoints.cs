c4e7e962fe341beed19349f825bd6c93b99b6219
Verchurro <berrygirault@gmail.com>
2023-02-16 18:08:02 +0100

    AGHHHHHHHH

--- CashierSim/Assets/Core/Scripts/Waypoints.cs
+++ CashierSim/Assets/Core/Scripts/Waypoints.cs
@@ -11,5 +11,12 @@ public class Waypoints : MonoBehaviour
             Gizmos.color = Color.cyan;
             Gizmos.DrawWireSphere(t.position, 1f);
         }
+        Gizmos.color= Color.red;
+        for(int i = 0; i < transform.childCount -1; i++)
+        {
+            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i + 1).position);
+        }
+
+        Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position, transform.GetChild(0).position);
     }
 }
